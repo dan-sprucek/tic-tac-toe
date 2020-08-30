cca51a9eb0bf45b23c9b5cfd6af78136ddf2ba9e
dan-sprucek <dan.sprucek@gmail.com>
2020-08-30 21:06:13 +0200

    Updates

---
+++ precache-manifest.f22b87e90391ad203f41212d3b348402.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8af04afdf7fddea0491b6597b5c0317",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "8e4f5830d7ed9763376b",
+    "url": "/tic-tac-toe/static/css/main.d7456329.chunk.css"
+  },
+  {
+    "revision": "d4beeb07b80aeb6b3f4c",
+    "url": "/tic-tac-toe/static/js/2.f47b7d6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.f47b7d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e4f5830d7ed9763376b",
+    "url": "/tic-tac-toe/static/js/main.27d8e761.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
